e656cec0cdf9e365746de1e3ce16540b16498579
hakobera <hakobera@gmail.com>
2011-12-03 12:29:11 +0900

    Add push stream feature using Socket.IO

--- app.js
+++ app.js
@@ -4,9 +4,12 @@
  */
 
 var express = require('express')
+  , sio = require('socket.io')
   , routes = require('./routes')
+  , TwitterStream = require('./lib/tstream');
 
-var app = module.exports = express.createServer();
+var app = module.exports = express.createServer(),
+    io = sio.listen(app);
 
 // Configuration
 
@@ -30,6 +33,17 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', routes.index);
+
+// Twitte Stream with Socket.IO
+var stream = new TwitterStream('a');
+stream.on('data', function(data) {
+  io.sockets.emit('data', data);
+});
+stream.open();
+
+io.sockets.on('connection', function (socket) {
+  console.log('connected %d', socket.id);
+});
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
